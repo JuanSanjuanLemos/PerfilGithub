ff34e0c30dc6f5eaf8d6c722cdf60ff7e01189a1
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-25 14:22:27 -0300

    :sparkles: Add variation to profile with linkedin
    #

--- components/devLinks/index.js
+++ components/devLinks/index.js
@@ -1,10 +1,25 @@
 import dataJSON from "../../objects/profileCard/getData.js";
 
 function createDevLinks(){
-    return /*html*/`
+    var re = new RegExp("^((linkedin.com\/))")
+    if(re.test(dataJSON.blog)){
+        return /*html*/`
+        
+        <div class="wrapper-links">
+            <a href="${dataJSON.html_url}" class="wrapper-img">
+                <img class="img-link" src="./img/logo-github.png" alt="Logo GitHub">
+            </a>
+            <a href="https://${dataJSON.blog}" class="wrapper-img">
+                <img class="img-link" src="./img/logo-linkedin.png" alt="Logo Linkedin">
+            </a>
+        </div>
+        `
+    }else{
+        return/*html*/`
         <a href="${dataJSON.html_url}" class="wrapper-img">
-            <img class="img-link" src="./img/logo-github.png" alt="Logo GitHub">
+                <img class="img-link" src="./img/logo-github.png" alt="Logo GitHub">
         </a>
-    `
+        `
+    }
 }
 export default createDevLinks;
