b6124bf8f67bc9825125069db9165e0a90e24536
JuanSanjuanLemos <juansanjuan.dev@gmail.com>
2022-02-28 14:18:53 -0300

    :bug: Fix bug
    #

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 import listDevs from "./objects/profileCard/addCard.js";
 
+let root = document.getElementById("root");
+root.innerHTML = listDevs;
+
 const addButton = document.getElementById("add-button");
 addButton.addEventListener('click',writeList);
 function writeList(event){
+    root.innerHTML = listDevs;
 
-    $root.insertAdjacentHTML(
-        'beforeend',
-        `
-        ${listDevs}
-    `)
 }
